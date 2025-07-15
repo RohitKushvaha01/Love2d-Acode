ff42ab745882b61ce8ed5088d9d0d16338c74270
Rohit Kushvaha <oldisg131@gmail.com>
2025-07-15 12:54:23 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,24 @@
+-- love.load is called once at the beginning when the game starts
+-- 'arg' contains command line arguments (if any)
 function love.load(arg)
-    -- Set window height and width otherwise everythibg will look streched or compressed
-    love.window.setMode(tonumber(arg[2]), tonumber(arg[1]))
-    love.window.resizable = true
+    -- You can initialize variables, load images, sounds, etc. here
 end
 
+-- love.update is called continuously (every frame)
+-- 'dt' stands for delta time, the time (in seconds) since the last frame
+-- Use this to update game logic, animations, physics, etc.
 function love.update(dt)
-    -- update
+    -- Example: Move objects, update timers, check collisions, etc.
 end
 
+-- love.draw is called every frame to draw things on the screen
 function love.draw()
-    -- Set background color
+    -- Clear the screen and set the background color (R, G, B, A)
     love.graphics.clear(0.4, 0.1, 0.6, 1)
 
-    -- Draw something
+    -- Set the drawing color to white (R, G, B)
     love.graphics.setColor(1, 1, 1)
+
+    -- Draw text on the screen at position (10, 10)
     love.graphics.print("Hello, world!", 10, 10)
 end
